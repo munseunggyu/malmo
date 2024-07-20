37d776a750f8189685503d94ef6a47a1defb7197
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-20 15:58:38 +0900

    build: 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import RQProvider from "@/providers/RQProvider";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body>
         <RQProvider>{children}</RQProvider>
       </body>
     </html>
