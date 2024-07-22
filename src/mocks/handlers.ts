77b3eb07bb093bcf217d4a98b8ca49248e4c4d9d
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-22 19:53:54 +0900

    feat: MSW 사용자 조회 mock api 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,17 +2,17 @@ import { http, HttpResponse } from "msw";
 
 const apiUrl = "http://localhost:9090/";
 export const handlers = [
-  http.get(apiUrl + "chat/rooms", () => {
-    return HttpResponse.json([
-      { tagId: 1, title: "제로초", count: 1264 },
-      { tagId: 2, title: "원초", count: 1264 },
-      { tagId: 3, title: "투초", count: 1264 },
-      { tagId: 4, title: "쓰리초", count: 1264 },
-      { tagId: 5, title: "포초", count: 1264 },
-      { tagId: 6, title: "파이브초", count: 1264 },
-      { tagId: 7, title: "식스초", count: 1264 },
-      { tagId: 8, title: "세븐초", count: 1264 },
-      { tagId: 9, title: "나인초", count: 1264 }
-    ]);
+  http.get(apiUrl + "user", ({ request }) => {
+    const url = new URL(request.url);
+    const id = parseInt(url.searchParams.get("id") as string) || 0;
+
+    return HttpResponse.json({
+      deleted: true,
+      deletedAt: "2024-07-22T10:08:11.370Z",
+      createdAt: "2024-07-22T10:08:11.370Z",
+      updatedAt: "2024-07-22T10:08:11.370Z",
+      userId: id,
+      email: "hello@naver.com"
+    });
   })
 ];
