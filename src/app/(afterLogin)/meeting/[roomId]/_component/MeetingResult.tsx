83ee59121e47b6196df805d4b86f944c1bdd84eb
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-25 02:35:44 +0900

    feat: 회의 결과 컴포넌트 추가

---
+++ src/app/(afterLogin)/meeting/[roomId]/_component/MeetingResult.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function MeetingResult() {
+  return (
+    <section
+      className=''
+      style={{
+        flex: 185,
+        height: "calc(100% - 100px)"
+      }}
+    >
+      <h3 className='border border-divider-1 border-x-0 px-[68px] py-[24px] subtitle1'>
+        회의 결과
+      </h3>
+      <section className='px-[40px] py-[20px] h-full'>
+        <p className='bg-bg-3 h-full rounded-md border border-main px-[40px] py-[20px]'>
+          내용
+        </p>
+      </section>
+    </section>
+  );
+}
