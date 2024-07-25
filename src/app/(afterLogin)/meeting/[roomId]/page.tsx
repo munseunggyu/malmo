b7c0da5589c3d75b502ce5b3abf2a32904e7840f
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-26 04:03:45 +0900

    feat: 회의방 페이지 추가 회의하기 모달 추가

--- src/app/(afterLogin)/meeting/[roomId]/page.tsx
+++ src/app/(afterLogin)/meeting/[roomId]/page.tsx
@@ -1,45 +1,38 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { constants } from "@/utils";
 import { useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import MeetingHeader from "./_component/MeetingHeader";
 import MeetingContents from "./_component/MeetingContents";
 import MeetingResult from "./_component/MeetingResult";
+import { useAiStream } from "@/hook/useAiStream";
+
+import ModalPortal from "@/components/ui/ModalPortal";
+import ModalContainer from "@/components/ui/ModalContainer";
+import ChatModal from "@/components/ChatModal";
+import { useModal } from "@/hook/useModal";
 
 interface IProps {
   params: { roomId: string };
 }
 
 export default function MeetingPage({ params }: IProps) {
-  const { data } = useSession();
-  const user = data?.user;
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  const { openModal, handleOpenMoal, handleCloseModal } = useModal();
 
   const searchParams = useSearchParams();
-  const chatPhaseId = searchParams.get("chatPhaseId") || "";
+  const phase = ((searchParams.get("phase") || "1") as "1") || "2" || "3";
+  const isNew = searchParams.get("isNew") || "";
 
-  const fetchAiStream = async ({
-    userId,
-    chatPhaseId
-  }: {
-    userId: string | undefined;
-    chatPhaseId: string;
-  }) => {
-    if (!userId) return;
-    const response = await fetch(constants.apiUrl + "chat/stream", {
-      method: "POST",
-      headers: {
-        accept: "text/event-stream",
-        "user-id": userId,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        chatPhaseId: Number(chatPhaseId),
-        role: "BLUE_HAT",
-        gptType: "HYPER_CLOVA"
-      })
-    });
-  };
+  const { sseMeetingData, setSseMeetingData } = useAiStream({
+    userId: user?.id,
+    roomId: params.roomId,
+    isNew,
+    phase
+  });
+
   return (
     <div className='h-full'>
       <MeetingHeader />
@@ -49,9 +42,24 @@ export default function MeetingPage({ params }: IProps) {
           height: "calc(100% - 130px)"
         }}
       >
-        <MeetingContents />
+        <MeetingContents
+          hats={sseMeetingData[phase].aiMessages}
+          handleOpenMoal={handleOpenMoal}
+          chatPhaseId3={sseMeetingData["3"].chatPhaseId}
+        />
         <MeetingResult />
       </section>
+      {openModal && (
+        <ModalPortal>
+          <ModalContainer handleCloseModal={handleCloseModal}>
+            <ChatModal
+              phase={phase}
+              roomId={params.roomId}
+              handleCloseModal={handleCloseModal}
+            />
+          </ModalContainer>
+        </ModalPortal>
+      )}
     </div>
   );
 }
