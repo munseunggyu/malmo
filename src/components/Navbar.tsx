11a0419f4c5108fc82073ef77470c40476315abe
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-27 02:33:37 +0900

    feat: Navbar 이미지 추가 및 링크 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,20 @@ import icoNew from "../../public/ico-new.svg";
 import icoStar from "../../public/ico-star.svg";
 import icoHistoryfile from "../../public/ico-historyfile.svg";
 import icoSetting from "../../public/ico-settings.svg";
+import mainLogo from "../../public/logo-malmo.png";
 import { signOut } from "next-auth/react";
 import { useModal } from "@/hook/useModal";
 import ModalPortal from "./ui/ModalPortal";
 import ModalContainer from "./ui/ModalContainer";
 import ChatModal from "./ChatModal";
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 export default function Navbar() {
   const { openModal, handleCloseModal, handleOpenMoal } = useModal();
+
+  const segment = useSelectedLayoutSegment();
+  console.log(segment);
 
   const handleSignOut = () => {
     signOut({
@@ -26,21 +31,35 @@ export default function Navbar() {
   return (
     <nav className='fixed flex flex-col bg-[#4f4f4f] h-full py-[20px] px-[10px] items-center'>
       <ul className='flex flex-col items-center '>
-        <li>Logo</li>
-        <li className='pt-[24px]'>
+        <li>
+          <Link href={"/"}>
+            <Image src={mainLogo} alt='말모말모' width={64} height={64} />
+          </Link>
+        </li>
+        <li className='pt-[24px] hover:opacity-[0.8]'>
           <button onClick={handleOpenMoal}>
             <Image src={icoNew} width={64} height={64} alt='채팅 방 추가' />
           </button>
         </li>
-        <li className='pt-[32px]'>
-          <button>
-            <Image src={icoStar} width={36} height={36} alt='즐겨찾기' />
-          </button>
+        <li className='pt-[20px] h-[64px] mb-2'>
+          <Link
+            href={"/star"}
+            className={`my-[14px] block w-[36px] h-[36px]  hover:bg-[url('/hoverStarImg.png')] hover:my-0 hover:w-[64px] hover:h-[64px] bg-cover bg-center ${
+              segment === "star"
+                ? "w-[64px] h-[64px] bg-[url('/activeStarImg.png')] my-0"
+                : "bg-[url('/ico-star.svg')]"
+            }`}
+          ></Link>
         </li>
-        <li className='pt-[28px]'>
-          <Link href={"/history"}>
-            <Image src={icoHistoryfile} width={36} height={36} alt='폴더' />
-          </Link>
+        <li className='pt-[20px]  h-[64px]'>
+          <Link
+            href={"/history"}
+            className={`my-[14px] block w-[36px] h-[36px]  hover:bg-[url('/hoverHistoryImg.png')] hover:my-0 hover:w-[64px] hover:h-[64px] bg-cover bg-center ${
+              segment === "history"
+                ? "w-[64px] h-[64px] bg-[url('/activeHistoryImg.png')] my-0"
+                : "bg-[url('/ico-historyfile.svg')]"
+            }`}
+          ></Link>
         </li>
       </ul>
       <div className='mt-auto'>
