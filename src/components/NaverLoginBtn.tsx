9053ae6f44d704a61b6a7bf2e2c97982c50ea149
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-26 16:11:14 +0900

    feat: 회원가입 모달 추가 및 로그인 버튼 디자인 변경

--- src/components/NaverLoginBtn.tsx
+++ src/components/NaverLoginBtn.tsx
@@ -1,10 +1,18 @@
 "use client";
+
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
+
+import naverImg from "../../public/naver-login.png";
 
 export default function NaverLoginBtn() {
   const handleSignup = async () => {
     signIn("naver", { redirect: true, callbackUrl: "/" });
   };
-  return <button onClick={handleSignup}>NaverLoginBtn</button>;
+  return (
+    <button onClick={handleSignup}>
+      <Image src={naverImg} alt='네이버 로그인' width={380} height={72} />
+    </button>
+  );
 }
---
+++ src/components/SignModal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { MouseEventHandler } from "react";
+import NaverLoginBtn from "./NaverLoginBtn";
+
+import closeImg from "../../public/ico-close.svg";
+import Image from "next/image";
+
+export default function SignModal() {
+  const handleStopPropgation: MouseEventHandler<HTMLDivElement> = e => {
+    e.stopPropagation();
+  };
+  return (
+    <div
+      className='rounded-md bg-bg-3 flex flex-col items-center justify-center w-[540px] h-[282px] relative'
+      onClick={handleStopPropgation}
+    >
+      <button className='absolute top-4 right-4'>
+        <Image src={closeImg} alt={"닫기"} width={24} height={24} />
+      </button>
+      <h3 className='text-[28px] font-[600]'>회원가입</h3>
+      <p className='pt-[12px] pb-md text-[20px] font-[400] text-center'>
+        간편하게 가입하고 <br />
+        멋진 아이디어를 함께 만들어요!
+      </p>
+      <NaverLoginBtn />
+    </div>
+  );
+}
