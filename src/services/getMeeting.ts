9a748f7a3ae4ad7f959d441a8c42a13135371bca
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-27 01:58:10 +0900

    refactory: 주석 제거

--- src/services/getMeeting.ts
+++ src/services/getMeeting.ts
@@ -16,20 +16,6 @@ export const getMeeting = async ({
   });
 
   const data = await response.json();
-  console.log(data);
-  // let result = { "1": {}, "2": {}, "3": {} };
-  // for (let i = 0; i < data.rooms.length; i++) {
-  //   const idx = String(i + 1) as "1" | "2" | "3";
-  //   result[idx] = {
-  //     ...data.rooms[i],
-  //     aiMessages: data.rooms[i].aiMessages.map((hat: IAllMessages) => ({
-  //       ...hat,
-  //       isFinish: true,
-  //       ...roleInfo(hat.role)
-  //     }))
-  //   };
-  // }
-  // console.log("wow", result);
 
   return data.rooms;
 };
