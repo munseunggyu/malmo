b7d77d09bd26d1403878940ba9b4d615744137d2
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-26 20:42:12 +0900

    feat: 캐러셀 이미지 추가 및 링크 추가

--- src/app/_component/LoginAfterFirstView.tsx
+++ src/app/_component/LoginAfterFirstView.tsx
@@ -3,7 +3,25 @@ import React from "react";
 import GuideGrid from "./GuideGrid";
 import CarouselUi from "@/components/ui/CarouselUi";
 
+import green from "../../../public/carousel/green.png";
+import black from "../../../public/carousel/black.png";
+import red from "../../../public/carousel/red.png";
+import blue from "../../../public/carousel/blue.png";
+import yellow from "../../../public/carousel/yellow.png";
+import white from "../../../public/carousel/white.png";
+import Image from "next/image";
+
 export default function LoginAfterFirstView() {
+  const link =
+    "https://daisy-hacksaw-0a3.notion.site/60055e40a24e4b749d580c9b7ec71457";
+  const carouselData = [
+    { img: green, link, name: "green" },
+    { img: black, link, name: "black" },
+    { img: red, link, name: "red" },
+    { img: blue, link, name: "blue" },
+    { img: yellow, link, name: "yellow" },
+    { img: white, link, name: "white" }
+  ];
   return (
     <div className='mt-[205px] mx-auto flex flex-col items-center'>
       <h2 className='title1 leading-[130%] mb-xs'>
@@ -15,13 +33,12 @@ export default function LoginAfterFirstView() {
       </p>
       <div className='flex gap-x-[12px]'>
         <GuideGrid />
-        <CarouselUi containerClass={"w-[295px] "}>
-          <div className='flex justify-center items-center'>1</div>
-          <div className='flex justify-center items-center'>2</div>
-          <div className='flex justify-center items-center'>3</div>
-          <div className='flex justify-center items-center'>4</div>
-          <div className='flex justify-center items-center'>5</div>
-          <div className='flex justify-center items-center'>6</div>
+        <CarouselUi containerClass={"w-[244px] "}>
+          {carouselData.map((item, idx) => (
+            <a href={item.link} key={idx} target='_blank' className='h-full'>
+              <Image src={item.img} alt={item.name} width={244} height={440} />
+            </a>
+          ))}
         </CarouselUi>
       </div>
     </div>
