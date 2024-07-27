6cd03b41a0699507fc1ccefd7792c5c21427a8b3
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-27 22:34:32 +0900

    feat: Loading css 변경

--- src/components/ui/AiMessageLoading.tsx
+++ src/components/ui/AiMessageLoading.tsx
@@ -2,6 +2,19 @@ import React from "react";
 
 import styles from "./aiMessageLoading.module.css";
 
-export default function AiMessageLoading() {
-  return <span className={styles.loader}></span>;
+export default function AiMessageLoading({
+  size = 50,
+  className
+}: {
+  size?: number;
+  className?: string;
+}) {
+  return (
+    <span
+      style={{
+        width: size
+      }}
+      className={`${styles.loader} ${className}`}
+    ></span>
+  );
 }
