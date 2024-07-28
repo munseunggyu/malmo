591f67e0f0a3f267d5ac0f6c148a6c736a48131d
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-28 13:15:54 +0900

    feat: naver, goole 검색 등록 및 metadata 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,28 @@ import { authOptions } from "@/auth";
 
 export const metadata: Metadata = {
   title: "말모말모",
-  description: "말모말모"
+  description:
+    "브레인스토밍을 돕는 여섯 모자들과 함께 아이디어를 확장시켜보아요.",
+  openGraph: {
+    title: "말모말모",
+    description:
+      "브레인스토밍을 돕는 여섯 모자들과 함께 아이디어를 확장시켜보아요.",
+    images: [
+      {
+        url: "https://github.com/user-attachments/assets/ad8f20c9-ec03-4778-932d-2186ee52dd42",
+        width: 1200,
+        height: 600
+      }
+    ],
+    siteName: "말모말모",
+    type: "website"
+  },
+  verification: {
+    google: "ENCP1b2GOOB9cZ_5jqglmVek2TNPce1L8PC0MfkEuKU",
+    other: {
+      "naver-site-verification": ["58265f3e1a282ded635f9c282f1c8dbf5dcbc10a"]
+    }
+  }
 };
 
 export default async function RootLayout({
